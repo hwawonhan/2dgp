e584a3268b247f84eff255f31f74fc111860906a
2dgp <hwawon22@naver.com>
2016-10-13 19:48:18 +0900

    coilsion

--- main_state.py
+++ main_state.py
@@ -4,7 +4,6 @@ import os
 from pico2d import*
 import game_framework
 import title_state
-
 name = "MainState"
 
 kirby = None
@@ -14,6 +13,9 @@ coin2 = None
 coin3 = None
 coin4 = None
 running = True
+
+
+
 
 class Kirby:
     def __init__(self):
@@ -47,53 +49,81 @@ class Kirby:
 
     def draw(self):
         self.image.clip_draw(self.frame * 71, 0 + (73 * self.imagey), self.charsize, self.charsize, self.x, self.y)
-
+        draw_rectangle(self.x - 35, self.y - 35, self.x + 35, self.y + 35)
 class Coin1:
+    global kirby
+
+    def coilsion(self):
+        for i in range(8):
+            left_a, right_a, top_a, bottom_a = kirby.x - 35, kirby.x + 35, kirby.y + 35, kirby.y - 35
+            left_b, right_b, top_b, bottom_b = self.position[i][0] - self.moveX - 25, self.position[i][0] - self.moveX + 25,\
+                                               self.position[i][1] + 25, self.position[i][1] - 25
+            print(left_a, right_a, top_a, bottom_a)
+
+            if left_b < right_a and left_b > left_a:
+                if top_b < top_a and top_b > bottom_a:
+                    self.position[i] = (self.position[i][0], self.position[i][1], True)
+                if bottom_b < top_a and bottom_b > bottom_a:
+                    self.position[i] = (self.position[i][0], self.position[i][1], True)
+            elif right_b < right_a and right_b > left_a:
+                if top_b < top_a and top_b > bottom_a:
+                    self.position[i] = (self.position[i][0], self.position[i][1], True)
+                if bottom_b < top_a and bottom_b > bottom_a:
+                    self.position[i] = (self.position[i][0], self.position[i][1], True)
+
     def __init__(self):
         self.position = []
         for i in range(8):
             if i < 4:
-                self.position.append((800 + (i * 50), 300 + (50 * i)))
+                self.position.append((800 + (i * 50), 300 + (50 * i), False))
             else:
-                self.position.append((800 + (i * 50), self.position[3][1] - (50 * (i-4))))
+                self.position.append((800 + (i * 50), self.position[3][1] - (50 * (i-4)), False))
         self.moveX = 0
         self.image = load_image('image//coin_1.png')
         self.imagex = 0
     def update(self):
         self.moveX += 10
         self.imagex = (self.imagex + 1) % 7
+        self.coilsion()
+
+
+
     def draw(self):
         for i in range(8):
-            self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
-
+            if self.position[i][2] == False:
+                self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+                draw_rectangle(self.position[i][0] - self.moveX - 25, self.position[i][1] - 25, self.position[i][0] - self.moveX + 25, self.position[i][1] + 25)
 class Coin2:
     def __init__(self):
         self.position = []
         for i in range(8):
             if i < 4:
-                self.position.append((1200 + (i * 50), 300 - (50 * i)))
+                self.position.append((1200 + (i * 50), 300 - (50 * i), False))
             else:
-                self.position.append((1200 + (i * 50), self.position[3][1] + (50 * (i-4))))
+                self.position.append((1200 + (i * 50), self.position[3][1] + (50 * (i-4)), False))
         self.moveX = 0
         self.image = load_image('image//coin_1.png')
         self.imagex = 0
     def update(self):
         self.moveX += 10
         self.imagex = (self.imagex + 1) % 7
+
     def draw(self):
         for i in range(8):
-            self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+            if self.position[i][2] == False:
+                self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+                draw_rectangle(self.position[i][0] - self.moveX - 25, self.position[i][1] - 25, self.position[i][0] - self.moveX + 25, self.position[i][1] + 25)
 
 class Coin3:
     def __init__(self):
         self.position = []
         for i in range(8):
             if i < 4:
-                self.position.append((1600 + (i * 50), 300 + (50 * i)))
-                self.position.append((1600 + (i * 50), 300 - (50 * i)))
+                self.position.append((1600 + (i * 50), 300 + (50 * i), False))
+                self.position.append((1600 + (i * 50), 300 - (50 * i), False))
             else:
-                self.position.append((1600 + (i * 50), 500 - (50 * (i-3))))
-                self.position.append((1600 + (i * 50), 100 + (50 * (i-3))))
+                self.position.append((1600 + (i * 50), 500 - (50 * (i-3)), False))
+                self.position.append((1600 + (i * 50), 100 + (50 * (i-3)), False))
         self.moveX = 0
         self.image = load_image('image//coin_1.png')
         self.imagex = 0
@@ -102,20 +132,22 @@ class Coin3:
         self.imagex = (self.imagex + 1) % 7
     def draw(self):
         for i in range(16):
-            self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+            if self.position[i][2] == False:
+                self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+                draw_rectangle(self.position[i][0] - self.moveX - 25, self.position[i][1] - 25, self.position[i][0] - self.moveX + 25, self.position[i][1] + 25)
 
 class Coin4:
     def __init__(self):
         self.position = []
         for i in range(8):
             if i < 4:
-                self.position.append((2000 + (i * 50), 300 + (50 * i)))
-                self.position.append((2000 + (i * 50), 300 - (50 * i)))
+                self.position.append((2000 + (i * 50), 300 + (50 * i), False))
+                self.position.append((2000 + (i * 50), 300 - (50 * i), False))
             else:
-                self.position.append((2000 + (i * 50), 500 - (50 * (i - 3))))
-                self.position.append((2000 + (i * 50), 100 + (50 * (i - 3))))
+                self.position.append((2000 + (i * 50), 500 - (50 * (i - 3)), False))
+                self.position.append((2000 + (i * 50), 100 + (50 * (i - 3)), False))
             if i != 0 and i != 7:
-                self.position.append((2000 + (i * 50), 300))
+                self.position.append((2000 + (i * 50), 300, False))
 
         self.moveX = 0
         self.image = load_image('image//coin_1.png')
@@ -125,7 +157,9 @@ class Coin4:
         self.imagex = (self.imagex + 1) % 7
     def draw(self):
         for i in range(22):
-            self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+            if self.position[i][2] == False:
+                self.image.clip_draw((self.imagex * 50), 0, 50, 50, self.position[i][0] - self.moveX, self.position[i][1])
+                draw_rectangle(self.position[i][0] - self.moveX - 25, self.position[i][1] - 25, self.position[i][0] - self.moveX + 25, self.position[i][1] + 25)
 
 class Sky:
     def __init__(self):
@@ -213,12 +247,15 @@ def handle_events():
                 kirby.imagenum = 8
 
 def update():
+    global coin1
     kirby.update()
     sky.update()
     coin1.update()
     coin2.update()
     coin3.update()
     coin4.update()
+
+
     delay(0.07)
 
 def draw():
