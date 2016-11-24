764c39ebf8beb2507047e50fda98b489b89bb3be
2dgp <hwawon22@naver.com>
2016-11-24 22:23:26 +0900

    title start update

--- title_state.py
+++ title_state.py
@@ -5,16 +5,21 @@ from pico2d import *
 
 name = "TitleState"
 image = None
-
+imagestart = None
+imagestarty = 0
+imagerank = None
+imageranky = 1
 def enter():
-    global image
-    image = load_image('image//title.png')
-
+    global image, imagestart, imagerank
+    image = load_image('image//title_.png')
+    imagestart = load_image('image//Start.png')
+    imagerank = load_image('image//Ranking.png')
 def exit():
     global image
     del(image)
 
 def handle_events():
+    global imagestarty, imageranky
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -22,14 +27,23 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
+                imagestarty = 0
+                imageranky = 1
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
+                imagestarty = 1
+                imageranky = 0
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-               game_framework.change_state(main_state)
+                if imagestarty == 0:
+                    game_framework.change_state(main_state)
 
 
 
 def draw():
     clear_canvas()
     image.draw(400, 300)
+    imagestart.clip_draw(0, 100 * imagestarty, 200, 100, 250, 100)
+    imagerank.clip_draw(0, 100 * imageranky, 200, 100, 550, 100)
     update_canvas()
 
 
