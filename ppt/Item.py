6649f90dff720d5ced55a58938d3e32856c7240b
2dgp <hwawon22@naver.com>
2016-12-18 20:07:29 +0900

    item image

---
+++ ppt/Item.py
@@ -0,0 +1,28 @@
+from pico2d import *
+from Kirby import Kirby
+import random
+
+class Monster1:
+    def __init__(self, i):
+        self.x, self.y = 800, (i+1) * 200
+        self.image = load_image('image//M3.png')
+        self.life_time = 0.0
+        self.apper = False
+        self.speed = 80
+        self.crush = False
+        self.remove = False
+
+    def update(self, current_time, frame_time, kirby):
+        self.life_time += frame_time
+        distance = self.speed * frame_time
+
+        if self.apper == False:
+            if self.life_time > 20:
+                self.apper = True
+        else:
+            if(self.x > 600):
+                self.x -= distance
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 60, 0 + (60 * self.imagey), 60, 60, self.x, self.y)
